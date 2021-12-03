f6c9aeafa881baa30c1f9db5dd79bc116c73734e
tejas-trivedi <500077185@stu.upes.ac.in>
2021-12-03 22:56:37 +0530

    deploy 1

--- questions/views.py
+++ questions/views.py
@@ -18,19 +18,19 @@ class QuestionsView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, ques_id, format=None):
-
-        data = request.get('https://tech-event-nov-21-default-rtdb.firebaseio.com/')
+        if firebase_admin._DEFAULT_APP_NAME not in firebase_admin._apps:
+            cred = credentials.Certificate(
+                'tech-event-nov-21-firebase-adminsdk-52bqf-9b37df2e76.json')
+            check = firebase_admin.initialize_app(cred, {
+                'databaseURL': 'https://tech-event-nov-21-default-rtdb.firebaseio.com/'
+            })
+        data = requests.get('https://tech-event-nov-21-default-rtdb.firebaseio.com/Questions/%s.json' %ques_id)
         data = data.json()
+
         question = data['question']
-        options = dict()
-
-        for i in range(1, len(data['choices'])):
-            options[i] = data['choices'][i]
-
         response_data = {
             "Question": question,
-            "Choices": options,
-        }
+            }
         return Response(response_data)
 
 
@@ -97,3 +97,82 @@ class CodeOutputResponseView(APIView):
         ref.update(code_data)
 
         return Response("Code output submitted")
+
+
+
+class CodeOutputCheckView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, ques_id, format=None):
+        if firebase_admin._DEFAULT_APP_NAME not in firebase_admin._apps:
+            cred = credentials.Certificate(
+                'tech-event-nov-21-firebase-adminsdk-52bqf-9b37df2e76.json')
+            check = firebase_admin.initialize_app(cred, {
+                'https://tech-event-nov-21-default-rtdb.firebaseio.com/'
+            })
+
+        uid = request.data.get("uid")
+        user = auth.get_user(uid)
+        data = user._data
+        sap = data.get('displayName')
+        main_ref = firebase_admin.db.reference('/')
+
+        my_code_res = main_ref.child('code_response/{}/{}/code_output_res'.format(sap, ques_id)).get()
+        code_ans = main_ref.child('Questions/{}/code_ans'.format(ques_id)).get()
+
+        if my_code_res==code_ans:
+            response_data = {
+                "Right Answer"
+            }
+        else:
+            response_data = {
+                "Wrong Answer Try Again"
+            }
+
+        if my_code_res==None or code_ans==None:
+            response_data = "Invalid"
+
+        return Response(response_data)
+
+
+class ScoreView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        if firebase_admin._DEFAULT_APP_NAME not in firebase_admin._apps:
+            cred = credentials.Certificate(
+                'tech-event-nov-21-firebase-adminsdk-52bqf-9b37df2e76.json')
+            check = firebase_admin.initialize_app(cred, {
+                'databaseURL': 'https://tech-event-nov-21-default-rtdb.firebaseio.com/'
+            })
+
+        uid = request.data.get("uid")
+        user = auth.get_user(uid)
+        data = user._data
+        sap = data.get('displayName')
+
+        main_ref = firebase_admin.db.reference('/')
+
+        score=0
+        for i in range(0, 7):
+            print(i)
+            correct_ans = main_ref.child('Questions/{}/correct_ans'.format(i)).get()
+            selected_ans = main_ref.child('responses/{}/{}/response'.format(sap, i)).get()
+
+            print(correct_ans, selected_ans)
+
+            if correct_ans==selected_ans:
+                score = score + 1
+                print(score)
+            else:
+                score = score
+
+        score_data = {
+            "Score": score
+        }
+
+        ref = main_ref.child('score/{}'.format(sap))
+        ref.update(score_data)
+
+        return Response(score_data)
+
